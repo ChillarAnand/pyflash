f17fbdf783ad8ff1cc2f6d74524cafd6263d1e5b
chillaranand <anand21nanda@gmail.com>
2016-10-28 15:32:52 +0530

    Add ak scan command

--- pyflash/cli.py
+++ pyflash/cli.py
@@ -1,8 +1,10 @@
 import os
+import time
 
 import click
+import requests
 
-from .utils import to_kindle, imp_mgc_fixup, imd_data
+from .utils import imd_data, imp_mgc_fixup, to_kindle
 
 
 @click.command()
@@ -47,3 +49,37 @@ def im_fixup():
 def imd(from_date, to_date, state):
     click.echo('Getting IMD data')
     imd_data(from_date, to_date, state)
+
+
+@cli.command()
+@click.option('--num', '-n', default=None)
+@click.option('--debug', '-d', default=False)
+def dynamic(num, debug):
+    if debug:
+        base_url = 'http://0.0.0.0:8000/'
+    else:
+        base_url = 'https://api.appknox.com/'
+    url = base_url + 'api/token/new.json'
+    print(url)
+    data = {
+        'username': os.environ.get('AK_USER'),
+        'password': os.environ.get('AK_PASS')
+    }
+    response = requests.post(url, data=data)
+
+    try:
+        data = response.json()
+        token = data['token']
+        user_id = data['user']
+    except:
+        print(response.text)
+        return
+
+    url = base_url + 'api/dynamic_shutdown/{}'.format(num)
+    print(url)
+    response = requests.post(url, data=data, auth=(user_id, token))
+    time.sleep(4)
+    url = base_url + 'api/dynamic/{}'.format(num)
+    print(url)
+    response = requests.post(url, data=data, auth=(user_id, token))
+    print(response.text)
