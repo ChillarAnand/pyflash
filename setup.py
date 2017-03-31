a85885db206346c9b0a794060915b0957b300666
chillaranand <anand21nanda@gmail.com>
2017-03-31 18:31:49 +0530

    Added missing fields in setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-"""
-My Tool does one thing, and one thing well.
-"""
 from setuptools import find_packages, setup
 
 dependencies = open('requirements.txt').read().splitlines()
+
+description = 'Get flash like superpowers by automating everyday tasks!'
+with open('README.rst') as fh:
+    long_description = fh.read()
+
 
 setup(
     name='pyflash',
@@ -12,36 +14,33 @@ setup(
     license='BSD',
     author='Chillar Anand',
     author_email='anand21nanda@gmail.com',
-    description='My Tool does one thing, and one thing well.',
-    long_description=__doc__,
+    description=description,
+    long_description=long_description,
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
     platforms='any',
     install_requires=dependencies,
+
     entry_points={
         'console_scripts': [
             'flash = pyflash.cli:cli',
         ],
     },
     classifiers=[
-        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        # 'Development Status :: 1 - Planning',
-        # 'Development Status :: 2 - Pre-Alpha',
-        # 'Development Status :: 3 - Alpha',
         'Development Status :: 4 - Beta',
-        # 'Development Status :: 5 - Production/Stable',
-        # 'Development Status :: 6 - Mature',
-        # 'Development Status :: 7 - Inactive',
+
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
+
         'Operating System :: POSIX',
         'Operating System :: MacOS',
         'Operating System :: Unix',
+
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
+
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
